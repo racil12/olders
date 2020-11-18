bbf3937b24e29edf42e7b40279b90ee6b800b77d
Salsabil <salsabil.racil@esprit.tn>
2020-11-18 19:26:17 +0100

    Initial Commit

---
+++ MiniProjetapp/MiniProjetappApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniProjetappApp.swift
+//  MiniProjetapp
+//
+//  Created by Salsabil Racil on 11/18/20.
+//
+
+import SwiftUI
+
+@main
+struct MiniProjetappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
